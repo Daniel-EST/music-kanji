436821797950e6127079159cda1d1bc226472a44
Daniel dos Santos <daniel.dsantos@b2wdigital.com>
2020-03-10 18:50:15 -0300

    fix tests utils-test

--- crawler/utils/__init__.py
+++ crawler/utils/__init__.py
@@ -4,7 +4,8 @@ import csv
 def parse_csv(csv_string):
     data = list(csv.reader(csv_string.split('\n')))
     labels = data[0]
-    rows = data[1:-1]
+    rows = data[1:]
 
     for row in rows:
-        yield dict(zip(labels, row))
+        if len(row) > 0:
+            yield dict(zip(labels, row))
--- tests/utils-test.py
+++ tests/utils-test.py
@@ -20,6 +20,15 @@ class TestUtils(unittest.TestCase):
                                          'column_2': 'e',
                                          'column_3': 'f'}])
 
+        csv_string = 'column_1,column_2,column_3\na,b,c\nd,e,f\n'
+        result = parse_csv(csv_string)
+        self.assertEqual(list(result), [{'column_1': 'a',
+                                         'column_2': 'b',
+                                         'column_3': 'c'},
+                                        {'column_1': 'd',
+                                         'column_2': 'e',
+                                         'column_3': 'f'}])
+
 
 if __name__ == '__main__':
     unittest.main()
