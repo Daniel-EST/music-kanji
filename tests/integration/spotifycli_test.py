3a4f8b926f398a437b1ea34646bc42734cd9b173
Daniel-EST <dd_santos@id.uff.br>
2020-03-13 00:18:41 -0300

    adds more tests

---
+++ tests/integration/spotifycli_test.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest.mock import patch
+
+from crawler.exceptions import InvalidCredentials
+from crawler.spotifycli import SpotifyClient
+
+
+class SpotifyClientTests(unittest.TestCase):
+    @patch("requests.post")
+    def test_auth_wrong_credentials(self, mock_post):
+        mock_post.return_value.status_code = 400
+        spotify_client = SpotifyClient("id", "secret")
+
+        with self.assertRaises(InvalidCredentials):
+            spotify_client.auth()
+
+
+if __name__ == '__main__':
+    unittest.main()
