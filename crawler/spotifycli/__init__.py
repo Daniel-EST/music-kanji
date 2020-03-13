74f74d25cb5e70216d30f1ad935269a304da0b21
Daniel-EST <dd_santos@id.uff.br>
2020-03-13 00:17:48 -0300

    adds more tests

--- crawler/spotifycli/__init__.py
+++ crawler/spotifycli/__init__.py
@@ -50,6 +50,3 @@ class SpotifyClientAuthenticated(SpotifyClient):
         response = requests.get('https://api.spotify.com/v1/search', params=params, headers=headers)
 
         return response.json()
-
-
-
